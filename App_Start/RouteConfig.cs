3b4f0b17c4c5abc3867faf057e92a46be9854b3e
Rikki Omas-as <riking98@hotmail.com>
2024-02-07 12:46:24 +0400

    Default page

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CPV_Mark3
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
