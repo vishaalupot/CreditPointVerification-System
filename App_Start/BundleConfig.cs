d4f7c9981f823dd0f850864bc5cf0cc5f69252b6
Rikki Omas-as <riking98@hotmail.com>
2024-02-23 12:35:29 +0400

    jquery 1

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace CPV_Mark3
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js","~/Scripts/jquery-ui-1.13.2.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
